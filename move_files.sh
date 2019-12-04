0d55efbeb218a1e801858083bc5e3fd7631bb9f3
rhamb001 <56095839+rhamb001@users.noreply.github.com>
2019-12-04 10:50:51 -0800

    Create move_files.sh

---
+++ move_files.sh
@@ -0,0 +1,36 @@
+#move results around and rename files
+cd kallisto_results 
+cd Glycerol_5.7_r1
+mv abundance.tsv g571.tsv
+cp g571.tsv ../..
+cd..
+
+cd Glycerol_5.7_r2
+mv abundance.tsv g572.tsv
+cp g572.tsv ../..
+cd ..
+
+cd Glycerol_7_r1
+mv abundance.tsv g71.tsv
+cp g71.tsv ../..
+cd ..
+
+cd Glycerol_7_r2
+mv abundance.tsv g72.tsv
+cp g72.tsv ../..
+cd .. 
+
+cd Pyruvate_5.7_r1
+mv abundance.tsv p571.tsv
+cp p571.tsv ../..
+cd ..
+
+cd Pyruvate_7_r1
+mv abundance.tsv p71.tsv
+cp p71.tsv ../..
+cd .. 
+
+cd Pyruvate_7_r2
+mv abundance.tsv p72.tsv
+cp p72.tsv ../..
+cd ../..
