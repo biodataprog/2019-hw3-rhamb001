e6fc5667da9967b4d2d2e0137b2614f18b942588
rhamb001 <56095839+rhamb001@users.noreply.github.com>
2019-12-04 16:15:17 -0800

    Create run_pfam.sh

---
+++ run_pfam.sh
@@ -0,0 +1,22 @@
+#this is code Dr. Stajich wrote
+#!/usr/bin/bash
+#SBATCH -p short -N 1 -n 8 --mem 8gb --out run_pfam.log
+
+CPUS=$SLURM_CPUS_ON_NODE
+if [ -z $CPUS ]; then
+ CPUS=1
+fi
+
+module load hmmer
+module load db-pfam
+
+IN=M_tuberculosis.pep.fasta
+DB=$PFAM_DB/Pfam-A.hmm
+BASE=$(basename $IN .pep.fasta)
+DOMTBL=$BASE.domtbl.tab
+OUT=$BASE.hmmscan
+if [ ! -f $DOMTBL ]; then
+    hmmscan --cpu $CPUS --domtbl $DOMTBL --tblout $BASE.tbl.tab --cut_ga -o $OUT $DB $IN 
+fi
+
+awk '{print $1,$2,$4}' $DOMTBL | grep -v '^#' > $BASE.domtbl_simple.tab
