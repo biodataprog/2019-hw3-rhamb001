a77b724c9f02020f3ebead1849d31c7e411f90c6
rhamb001 <56095839+rhamb001@users.noreply.github.com>
2019-12-04 16:19:46 -0800

    Update run_kallisto.sh

--- run_kallisto.sh
+++ run_kallisto.sh
@@ -1,6 +1,6 @@
-#this is code Dr. Stajich wrote
 #!/usr/bin/bash
 #SBATCH -p short -N 1 -n 8 --mem 8gb --out kallisto.log #this is how to run things on the cluster
+#this is code Dr. Stajich wrote
 
 CPUS=$SLURM_CPUS_ON_NODE
 if [ -z $CPUS ]; then
